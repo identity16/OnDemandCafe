6e35e2408d358f6091cd996ca1375389761a6091
identity16 <identity930@naver.com>
2018-11-10 22:38:14 +0900

    초기화면 index.fxml으로 세팅

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,15 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		FXMLLoader loader = new FXMLLoader();
 
-		Group root = new Group();
+		VBox root = FXMLLoader.load(getClass().getResource("/view/index.fxml"));
 		Scene scene = new Scene(root);
 
 		// Set Stage Properties
--- src/controller/IndexController.java
+++ src/controller/IndexController.java
@@ -1,5 +1,10 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class IndexController {
 
+	@FXML
+	public Label label1;
 }
