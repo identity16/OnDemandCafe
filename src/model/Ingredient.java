36107d6f3fd6adbd2b3540e51fa8b224781e15fa
dkile <dkile532@naver.com>
2018-11-11 16:38:14 +0900

    completed

--- src/model/Ingredient.java
+++ src/model/Ingredient.java
@@ -1,4 +1,30 @@
 package model;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.StringProperty;
+
 public class Ingredient {
+
+	private StringProperty name;
+	private IntegerProperty cost;
+	
+	public void setName(String name)
+	{
+		this.name.set(name);
+	}
+	
+	public void setCost(int cost)
+	{
+		this.cost.set(cost);
+	}
+	
+	public String getName()
+	{
+		return name.get();
+	}
+	
+	public int getCost()
+	{
+		return cost.get();
+	}
 }
