48be320e38763f29da218a5f4f16f3b7a504bc37
dkile <dkile532@naver.com>
2018-11-11 16:36:18 +0900

    completed

---
+++ src/controller/TitleViewController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import javafx.scene.control.Label;
+import javafx.util.Duration;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.animation.Animation;
+import javafx.animation.FadeTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+
+public class TitleViewController implements Initializable {
+	
+	@FXML private Label pressSpaceLabel;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources)
+	{
+		FadeTransition fadeTransition = new FadeTransition(Duration.seconds(2), pressSpaceLabel);
+		fadeTransition.setFromValue(0.0);
+		fadeTransition.setToValue(1.0);
+		fadeTransition.setCycleCount(Animation.INDEFINITE);
+		fadeTransition.play();
+	}
+}
