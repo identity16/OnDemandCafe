f492986613d8e6275a931fdea36b637b4b041edc
identity16 <identity930@naver.com>
2018-11-12 18:50:31 +0900

    Beverage 테스트 데이터 추가

--- src/controller/MenuController.java
+++ src/controller/MenuController.java
@@ -31,11 +31,31 @@ public class MenuController implements Initializable {
 	public void initialize(URL location, ResourceBundle resources) {
 		scrollPane.setStyle("-fx-background-color:transparent;");	// ScrollPane border 투명화
 
+		insertTestData();
+
 		List<Menu> menuList = MenuBoard.getInstance().getMenuList();
 
 		for(Menu menu : menuList) {
-			MenuControl control = new MenuControl(menu);
-			menuPane.getChildren().add(control);
+			menuPane.getChildren().add(new MenuControl(menu));
 		}
+
+	}
+
+	// 테스트 함수
+	private void insertTestData() {
+		MenuBoard menuBoard = MenuBoard.getInstance();
+
+		Beverage b1 = new Beverage(menuBoard.getMenu("아메리카노"));
+
+		Beverage b2 = new Beverage(menuBoard.getMenu("아메리카노"));
+		b2.setIngredientAmount("샷", 3);
+
+		Beverage b3 = new Beverage(menuBoard.getMenu("에스프레소 콘파냐"));
+		b3.setHot(true);
+		b3.setExtra(true);
+
+		orderedBeverages.add(b1);
+		orderedBeverages.add(b2);
+		orderedBeverages.add(b3);
 	}
 }
