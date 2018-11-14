911a8622f25ee0f7d530b733080970fde0e36e35
cjdthsus2gh <cjdthsus2gh@naver.com>
2018-11-14 18:47:18 +0900

    adminMenu 재료 리스트 나열

--- src/cafe/controller/AdminMenuController.java
+++ src/cafe/controller/AdminMenuController.java
@@ -1,18 +1,30 @@
 package cafe.controller;
 
 import cafe.SceneChanger;
+import cafe.controller.ui.IngredientControlFactory;
 import cafe.controller.ui.MenuControl;
+import cafe.model.Ingredient;
 import cafe.model.Menu;
 import cafe.model.MenuBoard;
+import javafx.application.Platform;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
 
 import java.net.URL;
 import java.util.List;
@@ -20,29 +32,63 @@ import java.util.ResourceBundle ;
 
 public class AdminMenuController implements Initializable {
 
+    @FXML private ListView<Ingredient> ingreListView;
+    @FXML private AnchorPane root;
+
     @FXML private Button btnCan, btnSave;
     @FXML private Button btnOpt;
     @FXML private TilePane menuPane;
-    @FXML private VBox ingrePane;
+
     @FXML private TextField editMenuPrice;
     @FXML private TextField editBasicPrice;
+    @FXML private TextField txtMenuField;
     @FXML private RadioButton radioBasic;
     @FXML private RadioButton radioCustom;
 
-    List<Menu> menuList = MenuBoard.getInstance().getMenuList();
+    private List<Menu> menuList = MenuBoard.getInstance().getMenuList();
+    private ObservableList<Ingredient> baseIngredientList;
+
+    private StringProperty menuNameProperty, menuPriceProperty;
+
+    private Boolean isMenuExist = true;
+
+    public AdminMenuController() {
+        baseIngredientList = FXCollections.observableArrayList();
+        menuNameProperty = new SimpleStringProperty(null);
+        menuPriceProperty = new SimpleStringProperty(null);
+    }
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        final ToggleGroup group = new ToggleGroup();
-        radioBasic.setToggleGroup(group);
-        radioCustom.setToggleGroup(group);
-        radioBasic.setSelected(true);
+        /* 재료 리스트 관련 이벤트 */
+/*
+        // 메뉴 이름,가격 텍스트필드 바인딩
+        menuNameProperty.bindBidirectional(txtMenuField.textProperty());
+        menuPriceProperty.bindBidirectional(editMenuPrice.textProperty());
+        //        // Event Handling
+        baseIngredientList.addListener((ListChangeListener<Ingredient>) c -> {			// 재료 변경 이벤트
+            // 첫 번째 Null(추가 버튼) 뺀 subList
+            List<Ingredient> withoutNull = baseIngredientList.subList(1, baseIngredientList.size());
 
-        editBasicPrice.setText(String.valueOf(Menu.getBasePrice()));
+            Menu existingMenu = MenuBoard.getInstance().getMenu(withoutNull);
+            isMenuExist = existingMenu != null;
 
+            if(!isMenuExist) {
+                editMenuPrice.clear();
+                txtMenuField.clear();
+            } else {
+                menuNameProperty.setValue(existingMenu.getName());
+                menuPriceProperty.setValue(""+existingMenu.getPrice());
+            }
+        });
+*/
+
+        /* 메뉴 리스트 관련 초기화 */
         listingMenu(menuList, false);
 
+        /* 버튼 이벤트 관련 초기화 */
         btnCan.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
@@ -52,6 +98,14 @@ public class AdminMenuController implements Initializable {
         btnSave.setOnAction(event -> handleSave(event));
         radioBasic.setOnAction(event -> handleBasic(event));
         radioCustom.setOnAction(event -> handleCustom(event));
+
+        /* 기타 옵션들 관련 초기화 */
+        final ToggleGroup group = new ToggleGroup();
+        radioBasic.setToggleGroup(group);
+        radioCustom.setToggleGroup(group);
+        radioBasic.setSelected(true);
+
+        editBasicPrice.setText(String.valueOf(Menu.getBasePrice()));
 
     }
 
@@ -65,19 +119,49 @@ public class AdminMenuController implements Initializable {
         Menu.setBasePrice(Integer.parseInt(basicPrice));
 
     }
-
     private void handleBasic(ActionEvent event) {
         listingMenu(menuList, false);
     }
     private void handleCustom(ActionEvent event) {
         listingMenu(menuList, true);
-    }
+    };
 
     private void listingMenu(List<Menu> menuList, boolean isCustom) {     //메뉴를 나열해주는 함수
         menuPane.getChildren().clear();
         for (Menu menu : menuList) {
-            if (isCustom == menu.isCustom())
-                menuPane.getChildren().add(new MenuControl(menu));
+            if (isCustom == menu.isCustom()) {
+
+                MenuControl control = new MenuControl(menu);
+                menuPane.getChildren().add(control);
+                control.rectangle.setOnMouseClicked(event -> {
+                    listingIngre(menu);
+                });
+            }
         }
+    }
+    private void listingIngre(Menu menu) {
+        // 값 초기화
+        Platform.runLater(() -> {
+            if(!menu.isDummy()) {
+                ingreListView.getItems().clear();
+                menuNameProperty.setValue(menu.getName());
+                baseIngredientList.add(0, new Ingredient());	// 추가 버튼 위치
+                baseIngredientList.addAll(menu.getBaseIngredients());
+            }
+        });
+
+        ingreListView.setItems(baseIngredientList);
+        ingreListView.setPlaceholder(new Label("재료가 없습니다."));
+        ingreListView.setCellFactory(new IngredientControlFactory());
+
+    }
+    private int calcBasePrice(List<Ingredient> baseIngredients) {
+        int price = Menu.getBasePrice();
+
+        for(Ingredient ingredient : baseIngredients) {
+            price += ingredient.getCost() * ingredient.getAmount();
+        }
+
+        return price;
     }
 }
